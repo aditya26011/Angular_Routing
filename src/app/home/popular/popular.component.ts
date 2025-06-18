6a2accd0859fb6e344cbfe3afafe9941f154eef6
Aditya <adityapawar826@gmail.com>
2025-06-18 22:12:05 +0530

    Programatically navigation

--- src/app/home/popular/popular.component.ts
+++ src/app/home/popular/popular.component.ts
@@ -1,4 +1,5 @@
 import { Component,inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Course } from 'src/app/Model/course';
 import { CourseService } from 'src/app/Services/course.service';
 
@@ -10,8 +11,15 @@ import { CourseService } from 'src/app/Services/course.service';
 export class PopularComponent implements OnInit {
 courseService = inject(CourseService)
 popularCourses: Course[] = [];
+router:Router=inject(Router)
+activeRoute:ActivatedRoute=inject(ActivatedRoute);
 
   ngOnInit(){
     this.popularCourses = this.courseService.courses.filter(c => c.rating >= 4.5);
+  }
+  navigateToCourses(){
+    //Both are absoulte path by default
+    // this.router.navigate(['courses'],{relativeTo: this.activeRoute});// Now this will be relative path
+    this.router.navigateByUrl('courses')
   }
 }
