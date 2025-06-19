bcd8046deda7dff198af65343a630a3e42e64ab8
Aditya <adityapawar826@gmail.com>
2025-06-19 21:49:41 +0530

    Reading queryString from URL

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,13 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CourseService } from '../Services/course.service';
 import { Course } from '../Model/course';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
 courseService=inject(CourseService);
-  AllCourses: Course[] = this.courseService.courses;
+activeRoute:ActivatedRoute=inject(ActivatedRoute);
 
+
+ AllCourses: Course[];
+ searchString:string;
+
+  ngOnInit(){
+  //  this.searchString= this.activeRoute.snapshot.queryParams['search'];
+     this.searchString= this.activeRoute.snapshot.queryParamMap.get('search');
+
+   console.log(this.searchString);
+   if(this.searchString===undefined || this.searchString===''||this.searchString===null){
+    this.AllCourses=this.courseService.courses;
+   }
+   else{
+    this.AllCourses=this.courseService.courses.      
+    filter(x=>x.title.toLowerCase().includes(this.searchString.toLowerCase()));
+   }
+  }
 }
