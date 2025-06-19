087b50c9c776a3fc9b36e9799fc16f7561414041
Aditya <adityapawar826@gmail.com>
2025-06-19 22:30:48 +0530

    Use of queryParamMap  observable instead of snapshot

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -5,27 +5,32 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
-export class CoursesComponent implements OnInit{
-courseService=inject(CourseService);
-activeRoute:ActivatedRoute=inject(ActivatedRoute);
+export class CoursesComponent implements OnInit {
+  courseService = inject(CourseService);
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
 
+  AllCourses: Course[];
+  searchString: string;
 
- AllCourses: Course[];
- searchString:string;
-
-  ngOnInit(){
-  //  this.searchString= this.activeRoute.snapshot.queryParams['search'];
-     this.searchString= this.activeRoute.snapshot.queryParamMap.get('search');
-
-   console.log(this.searchString);
-   if(this.searchString===undefined || this.searchString===''||this.searchString===null){
-    this.AllCourses=this.courseService.courses;
-   }
-   else{
-    this.AllCourses=this.courseService.courses.      
-    filter(x=>x.title.toLowerCase().includes(this.searchString.toLowerCase()));
-   }
+  ngOnInit() {
+    //  this.searchString= this.activeRoute.snapshot.queryParams['search'];
+    //  this.searchString= this.activeRoute.snapshot.queryParamMap.get('search');
+    this.activeRoute.queryParamMap.subscribe((data) => {
+      this.searchString = data.get('search');
+      console.log(this.searchString);
+      if (
+        this.searchString === undefined ||
+        this.searchString === '' ||
+        this.searchString === null
+      ) {
+        this.AllCourses = this.courseService.courses;
+      } else {
+        this.AllCourses = this.courseService.courses.filter((x) =>
+          x.title.toLowerCase().includes(this.searchString.toLowerCase())
+        );
+      }
+    });
   }
 }
