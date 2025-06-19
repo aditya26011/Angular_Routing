11f99f411089972deccad50d21916a4461f8b4c1
Aditya <adityapawar826@gmail.com>
2025-06-19 23:12:12 +0530

    Using frag to jump to a place .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+ activateRoute:ActivatedRoute=inject(ActivatedRoute);
+  ngOnInit(){
+    this.activateRoute.fragment.subscribe((frag)=>{
+      console.log(frag);
+      this.JumpToScare(frag);
+    })
+    
+  }
+  JumpToScare(section){
+      document.getElementById(section).scrollIntoView({behavior:'smooth'});
+    }
 
 }
