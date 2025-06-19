b0a408311a91b653c632dbaefae2eac59a7cec08
Aditya <adityapawar826@gmail.com>
2025-06-19 23:34:50 +0530

    Implementing child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,20 @@ import { ContactComponent } from './contact/contact.component';
 import { CoursesComponent } from './courses/courses.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
+import { PopularComponent } from './home/popular/popular.component';
 
   const routes: Routes = [
   // { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: '', component:HomeComponent },
+  { path: '', component:HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'courses', component: CoursesComponent },
-  { path: 'courses/course/:id', component: CourseDetailComponent },
-
+  // { path: 'courses/course/:id', component: CourseDetailComponent },
+  {path:'courses',children:[
+    {path:'course/:id',component:CourseDetailComponent},
+    {path:'popular',component:PopularComponent}
+  ]},
   { path: '**', component: NotfoundComponent } // fallback route
 ];
 
